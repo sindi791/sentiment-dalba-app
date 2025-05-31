f33aec19c1935d2e7de11da60da9f701979358f7
sindi791 <ncendekiar@gmail.com>
2025-05-31 23:45:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,50 @@
+# Buat ulang file app.py versi final tanpa error indentasi atau key
+final_app_code = """
 import streamlit as st
 import pandas as pd
 import pickle
 from utils import preprocess_text
 
-# Load model dan vectorizer
-model_svm = pickle.load(open('model_svm.pkl', 'rb'))
-model_nb = pickle.load(open('model_nb.pkl', 'rb'))
-vectorizer = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
-
-# Judul aplikasi
 st.title("Analisis Sentimen Komentar Instagram Produk D'Alba")
 
-# Pilih input
-option = st.radio("Pilih metode input komentar:", ['Manual', 'Upload CSV'])
+uploaded_file = st.file_uploader("Upload file komentar (.xlsx / .csv)", type=["xlsx", "csv"])
+model_choice = st.selectbox("Pilih Model Klasifikasi:", ["SVM", "Naive Bayes"])
 
-# Input manual
-if option == 'Manual':
-    text_input = st.text_area("Masukkan komentar:")
-    if text_input:
-        data = pd.DataFrame([text_input], columns=['komentar'])
+if uploaded_file:
+    if uploaded_file.name.endswith(".xlsx"):
+        data = pd.read_excel(uploaded_file)
     else:
-        st.stop()
-
-# Input dari file CSV
-else:
-    uploaded_file = st.file_uploader("Upload file CSV", type=["csv"])
-    if uploaded_file:
         data = pd.read_csv(uploaded_file)
+
+    # Cek apakah kolom komentar tersedia
+    kolom_komentar = None
+    for col in data.columns:
+        if col.lower() in ['komentar', 'comment']:
+            kolom_komentar = col
+            break
+
+    if not kolom_komentar:
+        st.error("File harus memiliki kolom bernama 'komentar' atau 'comment'")
     else:
-        st.stop()
+        if st.button("Klasifikasikan"):
+            data['cleaned'] = data[kolom_komentar].astype(str).apply(preprocess_text)
 
-# Pilih model
-model_choice = st.selectbox("Pilih Model Klasifikasi:", ['SVM', 'Naive Bayes'])
+            vectorizer = pickle.load(open("tfidf_vectorizer.pkl", "rb"))
+            X = vectorizer.transform(data['cleaned'])
 
-# Tombol klasifikasi
-if st.button("Klasifikasikan"):
-    # Preprocessing
-kolom_komentar = 'komentar' if 'komentar' in data.columns else 'comment'
-data['cleaned'] = data[kolom_komentar].astype(str).apply(preprocess_text)
+            if model_choice == "SVM":
+                model = pickle.load(open("model_svm.pkl", "rb"))
+            else:
+                model = pickle.load(open("model_nb.pkl", "rb"))
 
-    X = vectorizer.transform(data['cleaned'])
+            data['sentimen'] = model.predict(X)
+            st.write(data[[kolom_komentar, 'sentimen']])
+            st.bar_chart(data['sentimen'].value_counts())
+"""
 
-    # Prediksi
-    if model_choice == 'SVM':
-        prediction = model_svm.predict(X)
-    else:
-        prediction = model_nb.predict(X)
+# Simpan ke file app.py
+fixed_app_file = "/mnt/data/app.py"
+with open(fixed_app_file, "w") as f:
+    f.write(final_app_code)
 
-    # Tampilkan hasil
-    data['sentimen'] = prediction
-    st.write("Hasil Klasifikasi:")
-    st.dataframe(data[['komentar', 'sentimen']])
-
-    # Visualisasi
-    st.subheader("Visualisasi Sentimen")
-    st.bar_chart(data['sentimen'].value_counts())
+fixed_app_file
